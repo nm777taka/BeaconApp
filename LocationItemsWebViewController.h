d1230c3f26db6152e6bbab07ed3f3bb10901d9a0
nm777taka <nm7.taka@gmail.com>
2014-05-28 12:07:49 +0900

    first commit

---
+++ LocationItemsWebViewController.h
@@ -0,0 +1,13 @@
+//
+//  LocationItemsWebViewController.h
+//  LocationItems
+//
+//  Created by TakahisaFuruta on 2014/05/23.
+//  Copyright (c) 2014年 TakahisaFuruta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LocationItemsWebViewController : UIViewController
+
+@end
